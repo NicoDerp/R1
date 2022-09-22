1dcd8e9006b04e751bf2c2f36127a4c471d4e534
NicoDerp <derp.nico@gmail.com>
2022-09-22 20:37:53 +0200

    func

--- func.py
+++ func.py
@@ -1,10 +1,21 @@
 
 from pylab import *
+import types
 
+
+__functions = {}
 
 class Function():
-    def __init__(self, func, space=(-10, 10, 1001), dx=0.001):
-        self.func = func
+    def __init__(self, func, space=(-10, 10, 1001), dx=0.001, label=None):
+        if isinstance(func, str):
+            self.func = eval("lambda x:" + func, np.__dict__)
+            self.funcs = func
+        elif isinstance(func, types.FunctionType):
+            self.func = func
+            self.funcs = None
+        else:
+            raise ValueError("Func isn't string or lambda!")
+
         self.space = space
 
         if len(space) != 3:
@@ -12,47 +23,61 @@ class Function():
 
         self.lin = linspace(*space)
         self.dx = dx
+    
+        self.label = label
 
+        __functions.append(self)
+        
     def __call__(self, x):
         return self.func(x)
 
     def __ddx(self):
         f = lambda x: (self.func(x + self.dx) - self.func(x)) / self.dx
-        return self.copy().setFunc(f)
+        func = self.copy().setFunc(f)
+        if func.label:
+            func.label += "'"
+        return func
+
+    def __getname(self):
+        if self.label != None:
+            return self.label
+
+        l = [k for k,v in globals().items() if v is self]
+        self.label = l[0] if len(l) != 0 else ""
+        return self.label
 
     def ddx(self, n=1):
+        self.__getname()
         f = self
         for i in range(n):
             f = f.__ddx()
         return f
 
     def copy(self):
-        return Function(func=self.func, space=self.space, dx=self.dx)
+        return Function(func=self.func, space=self.space, dx=self.dx, label=self.label)
 
     def setFunc(self, func):
         self.func = func
         return self
 
     def plot(self):
-        plot(self.lin, self.func(self.lin))
-
-def f(x):
-    return 2*x**3 - 6*x**2 - 2*x + 6
-
-def ddx(x, func):
-    dx = 0.001
-    return (func(x+dx) - func(x)) / dx
+        l = self.__getname()
+        if l:
+            plot(self.lin, self.func(self.lin), label=l)
+        else:
+            plot(self.lin, self.func(self.lin))
 
 
-myFunc = Function(lambda x: x**2)
+f = Function("f(x) = 2*x**3 - 6*x**2 - 2*x + 6")
+g = Function("g(x) = x**2")
 
-X = np.linspace(-10, 10, 1001)
-Y = f(X)
+f.plot()
+f.ddx().plot()
+f.ddx(2).plot()
 
-myFunc.plot()
-myFunc.ddx().plot()
-myFunc.ddx(2).plot()
+g.plot()
 
-grid()
+legend()
+igrid()
 show()
 
