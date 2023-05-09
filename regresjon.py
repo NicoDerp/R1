14b061ef3b6b14ec030e40260b608ae41aa4f15c
Nico <52355359+NicoDerp@users.noreply.github.com>
2023-05-09 21:03:22 +0200

    Update regresjon.py

--- regresjon.py
+++ regresjon.py
@@ -2,31 +2,61 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import re
 
 
 class Function:
-    def __init__(self, f, coeffs, x, y):
+    def __init__(self, f, x=None, y=None, multiVariable=False):
         self.f = f
-        self.coeffs = coeffs
         self.x = x
         self.y = y
+        self.multiVariable = multiVariable
     
     def prettify(self):
         return "ooga booga"
     
+    def prettifyLatex(self):
+        return f"${self.prettify()}$"
+    
     def __call__(self, x):
         return self.f(x)
 
-class Polynomial(Function):
-    def __init__(self, coeffs, degree, x, y):
-        self.f = lambda x: sum([coeffs[i]*x**i for i in range(degree+1)])
-        super().__init__(self.f, coeffs, x, y)
-        self.degree = degree
+class Linear(Function):
+    def __init__(self, a, b, x=None, y=None):
+        self.f = lambda x: a*x + b
+        self.a = a
+        self.b = b
+        self.degree = 1
+        super().__init__(self.f, x, y)
     
     def prettify(self):
-        s = "$"
+        if round(self.a, 2) == 1.0:
+            s = "x"
+        else:
+            s = f"{self.a:.2f}x"
+        
+        if round(self.b, 2) == 0:
+            pass
+        elif self.b < 0:
+            s += f" - {-self.b:.2f}"
+        else:
+            s += f" + {self.b:.2f}"
+        return s
+
+class Polynomial(Function):
+    def __init__(self, coeffs, degree, x=None, y=None):
+        self.f = lambda x: sum([coeffs[i]*x**i for i in range(degree+1)])
+        self.coeffs = coeffs
+        self.degree = degree
+        super().__init__(self.f, x, y)
+    
+    def prettify(self):
+        s = ""
         for i in range(self.degree, -1, -1):
             c = self.coeffs[i]
+            if c == 0:
+                continue
+            
             if c < 0:
                 c = -c
                 if i == self.degree:
@@ -35,18 +65,145 @@ class Polynomial(Function):
                     s += " - "
             elif i != self.degree:
                 s += " + "
-    
+            
             if i == 0:
                 s += f"{c:.2f}"
             elif i == 1:
-                s += f"{c:.2f}x"
+                if round(c, 2) == 1.0:
+                    s += "x"
+                else:
+                    s += f"{c:.2f}x"
             else:
-                s += f"{c:.2f}x^{i}"
-        s += "$"
+                if round(c, 2) == 1.0:
+                    s += f"{c:.2f}x^{i}"
+                else:
+                    s += f"x^{i}"
+
         return s
 
+class Equation:
+    def __init__(self, *args):
+        if not isinstance(args[0], Equation):
+            l = args[0].split("=")
+            if len(l) != 2:
+                print("[ERROR] Cannot parse equation because it doesn't contain '='")
+                exit()
+            
+            f, g = l
+            self.f = parsePolynomial(f)
+            self.g = parsePolynomial(g)
+        else:
+            self.f = args[0]
+            self.g = args[1]
+    
+    def solve(self, maxIterations=100):
+        dx = 10**-6
+        func = lambda x: self.f(x) - self.g(x)
+        dfunc = lambda x: (func(x + dx) - func(x - dx)) / (2*dx)
+        xn = 0
+        for i in range(maxIterations):
+            xn = xn - func(xn) / dfunc(xn)
+        return xn
+    
+    def prettify(self):
+        return f"{self.f.prettify()} = {self.g.prettify()}"
+    
+    def prettifyLatex(self):
+        return f"${self.f.prettify()} = {self.g.prettify()}$"
 
-def linear(X, Y, maxIterations=10000):
+def parsePolynomial(s):
+    coeffs = {}
+    
+    # 2*x^2 + 1x - 3
+    matches = re.findall((
+            # Mellomrom +- mellomrom
+            r"[\s]*(-|\+)?[\s]*"
+            
+            r"(?:"
+                # Første mulighet
+                r"(?:"
+                    # Optional desimaltall eller et tall
+                    r"(\d+\.\d+|\d+|)?"
+                    
+                    # Et optional gangetegn
+                    r"\*?"
+                    
+                    # Optional konstanter (bokstaver)
+                    r"[a-zA-Z]*"
+            
+                    # Et optional gangetegn
+                    r"\*?"
+            
+                    # x med optional (**|^)
+                    r"(?:"
+                        # x
+                        r"x"
+                        
+                        # Optional
+                        r"(?:"
+                            # ^ eller **
+                            r"(?:\^|\*\*)"
+                            
+                            # Et tall
+                            f"(\d+)"
+                        r")?"
+                    r")"
+                r")"
+                
+                r"|"
+                    
+                # Andre mulighet
+                r"("
+                    # Et desimaltall eller tall
+                    r"\d+\.\d+|\d+"
+                    
+                    # Optional konstanter (bokstaver)
+                    r"[a-zA-Z]*"
+                ")"
+
+            r")"), s)
+            
+    print(matches)
+    
+    # ax^b
+    for match in matches:
+        sign = match[0]
+        a = "1"
+        if match[1]:
+            a = match[1]
+            b = match[2]
+        if match[3]:
+            a = match[3]
+            b = "0"
+        elif not match[2]:
+            b = "1"
+        a = float(sign + a)
+        b = float(b)
+        print(match, f"{a}x^{b}")
+        
+        if b in coeffs:
+            coeffs[b].append(a)
+        else:
+            coeffs[b] = [a]
+    
+    print(coeffs)
+    degree = len(coeffs)-1
+    if degree == 1:
+        return Linear(coeffs[1], coeffs[0])
+    return Polynomial([coeffs[i] for i in range(degree+1)], degree)
+                     
+def parseEquation(s):
+    l = s.split("=")
+    if len(l) != 2:
+        print("[ERROR] Cannot parse equation because it doesn't contain '='")
+        exit()
+    
+    f, g = l
+    f = parsePolynomial(f)
+    g = parsePolynomial(g)
+    return Equation(f, g)
+
+def linear(X, Y, maxIterations=100000):
     a, b = [0, 0]
     learning_rate = 0.001
     m = float(len(Y))
@@ -63,12 +220,11 @@ def linear(X, Y, maxIterations=10000):
         
         if i % 100 == 0:
             print(error, i)
-        #print(Y)
         
         if error < 0.000001:
             break
     
-    return Polynomial([b, a], 1, X, Y)
+    return Linear(a, b, X, Y)
 
 
 def polynomialAdaDelta(degree, X, Y, maxIterations=100000):
@@ -154,7 +310,54 @@ def polynomial(degree, X, Y, maxIterations=100000):
             print(error)
         #print(Y)
         
-        if error < 0.0001:
+        if error < 0.000001:
+            break
+        
+        if any(np.isnan(coeffs)):
+            print("[ERROR] Got NaN, try another function or supply more points")
+            exit()
+        
+        if error == lastError:
+            break
+            
+        lastError = error
+
+    return Polynomial([[c] for c in coeffs], degree, X, Y)
+
+def solveEquations(equations, maxIterations=100000):
+    coeffs = [0 for i in range(degree+1)]
+    #learning_rate = 0.00001
+    learning_rate = 0.001
+    B1 = 0.9
+    B2 = 0.999
+    epsilon = 10 ** -8
+    
+    m = float(len(Y))
+    Mt = np.zeros(degree+1)
+    Vt = np.zeros(degree+1)
+    lastError = 0
+    for i in range(maxIterations):
+        predicted = [sum([coeffs[i]*x**i for i in range(degree+1)]) for x in X]
+        
+        Ds = np.array([-2 * sum(X**i * (Y - predicted)) / m for i in range(degree+1)])
+        error = sum((Y - predicted) ** 2) / m
+        
+        Mt = B1*Mt + (1 - B1) * Ds
+        Vt = B2*Vt + (1 - B2) * Ds**2
+        
+        Mht = Mt / (1 - B1)
+        Vht = Vt / (1 - B2)
+        
+        # Adam numpizized
+        Ms = (learning_rate / np.sqrt(Vht) + epsilon) * Mht
+        
+        coeffs = [coeffs[i] - Ms[i] for i in range(degree+1)]
+        #print(error, coeffs)
+        if i % 100 == 0:
+            print(error)
+        #print(Y)
+        
+        if error < 0.000001:
             break
         
         if any(np.isnan(coeffs)):
@@ -173,8 +376,10 @@ def plotFunction(func, points=True):
     x = np.linspace(-20, 20, 51)
     y = func(x)
     
-    plt.plot(x, y, label=func.prettify())
-    plt.plot(func.x, func.y, 'o')
+    plt.plot(x, y, label=func.prettifyLatex())
+    
+    if func.x and func.y:
+        plt.plot(func.x, func.y, 'o')
     
     plt.grid()
     plt.legend()
@@ -182,7 +387,7 @@ def plotFunction(func, points=True):
 
 
 def f(x):
-    return 2*x + 1
+    return 2*x + np.sin(x*10**16)*10
 
 def f2(x):
     return 2*x**2 - 5*x + 2
@@ -191,9 +396,16 @@ def f3(x):
     return 6*x**3 - 2*x**2 - 5*x + 2
 
 testx = np.linspace(-10, 10, 11)
-testy = f(testx)
+testy = f2(testx)
 
-#plotFunction(polynomial(3, testx, testy))
-plotFunction(linear(testx, testy))
-plt.show()
+a = Equation("2ab = 16")
+b = Equation("a - b = 2")
+print("Solving", eq.prettify())
+answer = solveEquations([a, b])
+print(answer)
+
+#plotFunction()
+
+#plotFunction(polynomial(2, testx, testy))
+#plt.show()
 
