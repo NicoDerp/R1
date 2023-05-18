815f7df314a4bd6a428385a3d425f187ae1a11a5
NicoDerp <derp.nico@gmail.com>
2023-05-18 18:36:04 +0200

    LSTM feed forward working i think

--- ShatGPT.py
+++ ShatGPT.py
@@ -1,4 +1,4 @@
-
+import numpy
 import numpy as np
 
 
@@ -18,25 +18,37 @@ def dReLU(x):
     return max(0, 1)
 
 
-def softmax(x):
+def Softmax(x):
     e = np.exp(x - np.max(x))
     return e / np.sum(e)
+
+
+def Sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def dSigmoid(x):
+    s = Sigmoid(x)
+    return s * (1 - s)
 
 
 class Layer:
     def __init__(self, size, prevSize, activation):
         self.size = size
         self.prevSize = prevSize
-        self.state = np.zeros(size)
-        self.biases = np.zeros(size)
+        self.neurons = np.zeros(size)
+        self.states = np.zeros(size)
 
-        # Prev to this
-        self.xWeights = np.random.rand(size, prevSize)
+        self.fWeights = np.random.rand(size, size+prevSize)
+        self.iWeights = np.random.rand(size, size+prevSize)
+        self.cWeights = np.random.rand(size, size+prevSize)
+        self.oWeights = np.random.rand(size, size+prevSize)
 
-        # This to this
-        self.hWeights = np.random.rand(size, prevSize)
-        #self.weights = np.ones((next.size, size))
-        
+        self.fBiases = np.zeros(size)
+        self.iBiases = np.zeros(size)
+        self.cBiases = np.zeros(size)
+        self.oBiases = np.zeros(size)
+
         if activation == "none":
             self.activation = NoneActivation
             self.dActivation = dNoneActivation
@@ -48,29 +60,34 @@ class Layer:
 class LSTM:
     def __init__(self):
         self.inputSize = 2
-        self.layers = [Layer(4, self.inputSize, "none"),
-                       Layer(5, 4, "none"),
-                       Layer(2, 5, "none")]
-        self.layerCount = len(self.layers)
+        self.hiddenLayers = [Layer(4, self.inputSize, "none"),
+                             Layer(2, 4, "none")]
 
     def feedForward(self, inputState):
         if inputState.shape != (self.inputSize,):
             print(f"[ERROR] Feed-forward input's shape is not ({self.inputSize}, 0) but {inputState.shape}")
             return
 
-        curL = self.layers[0]
-        print(curL.hWeights.dot(inputState))
-        curL.state = curL.activation(curL.xWeights.dot(inputState) + curL.hWeights.dot(inputState) + curL.biases)
+        layer = self.hiddenLayers[0]
+        vt = np.concatenate((layer.states, inputState))
+        # for state in layer.states:
+        ft = Sigmoid(layer.fWeights.dot(vt) + layer.fBiases)
+        it = Sigmoid(layer.iWeights.dot(vt) + layer.iBiases)
+        ct = np.tanh(layer.cWeights.dot(vt) + layer.cBiases)
+        ot = Sigmoid(layer.oWeights.dot(vt) + layer.oBiases)
+        layer.neurons = ft * layer.neurons + it * ct
+        layer.states = ot * np.tanh(layer.neurons)
 
-        # 1 .. L-1
-        for i in range(1, self.layerCount-1):
-            prevL = self.layers[i-1]
-            curL = self.layers[i]
-            # nextL.zNeurons = curL.weights.dot(curL.neurons) + nextL.biases
-            curL.state = curL.activation(curL.xWeights.dot(prevL.state) + curL.hWeights.dot(prevL.state) + curL.biases)
-
-        # Calculate last layer which doesn't have hidden state
-        #self.layers[-1].state = nextL.activation(curL.xWeights.dot(curL.state) + curL.hWeights.dot(nextL.neurons))
+        for i in range(1, len(self.hiddenLayers)):
+            layer = self.hiddenLayers[i]
+            vt = np.concatenate((layer.states, self.hiddenLayers[i-1].neurons))
+            # for state in layer.states:
+            ft = Sigmoid(layer.fWeights.dot(vt) + layer.fBiases)
+            it = Sigmoid(layer.iWeights.dot(vt) + layer.iBiases)
+            ct = np.tanh(layer.cWeights.dot(vt) + layer.cBiases)
+            ot = Sigmoid(layer.oWeights.dot(vt) + layer.oBiases)
+            layer.neurons = ft * layer.neurons + it * ct
+            layer.states = ot * np.tanh(layer.neurons)
 
     def gradientDescent(self, actual):
         lossDerivative = (2.0/self.layers[-1].size) * (self.layers[-1].neurons - actual)
@@ -86,7 +103,10 @@ class LSTM:
 ai = LSTM()
 
 ai.feedForward(np.ones(ai.inputSize))
-print(ai.layers[0].state)
+for layer in ai.hiddenLayers:
+    print(layer.states)
+    print(layer.neurons)
+    print()
 #ai.gradientDescent(np.array([1, 2, 3, 4]))
 
 # print(a.neurons)
